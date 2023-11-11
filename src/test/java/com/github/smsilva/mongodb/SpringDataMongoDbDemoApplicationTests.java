709df83e3ce1528cfac0e61c92ed59c5fef58253
Silvio Silva <smsilva@gmail.com>
2023-11-11 20:16:11 -0300

    feat: Initial setup

---
+++ src/test/java/com/github/smsilva/mongodb/SpringDataMongoDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.smsilva.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongoDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
