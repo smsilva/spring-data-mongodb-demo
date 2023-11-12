51fea2f80fae2b4d1c1275d5ca3768e2fc558dff
Silvio Silva <smsilva@gmail.com>
2023-11-11 21:47:00 -0300

    feat: Update Database and User creation

---
+++ docker-entrypoint-initdb.d/dev.js
@@ -0,0 +1,18 @@
+db = db.getSiblingDB('dev');
+
+db.createUser({ user: 'developer', pwd: 'local', roles: [{ role: 'readWrite', db: 'dev' }]});
+
+db.createCollection('movies');
+
+db.movies.insertMany(
+  [
+    {"name":"The silence of the lambs"},
+    {"name":"The Godfather"},
+    {"name":"The Shawshank Redemption"},
+    {"name":"Pulp Fiction"},
+    {"name":"The Good, the Bad and the Ugly"},
+    {"name":"The Dark Knight"},
+    {"name":"Mission Impossible"},
+    {"name":"The Matrix"},
+  ]
+);
